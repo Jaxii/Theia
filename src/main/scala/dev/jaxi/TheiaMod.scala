d07e52d970a1207ead92b47779563c760c54eec0
BuildTools <jaxmontelongo@gmail.com>
2021-05-26 10:12:49 -0500

    scala

---
+++ src/main/scala/dev/jaxi/TheiaMod.scala
@@ -0,0 +1,17 @@
+package dev.jaxi
+
+import net.fabricmc.api.ModInitializer
+
+
+class TheiaMod extends ModInitializer {
+
+
+	override def onInitialize(): Unit = {
+
+		print("it works!!!!!!!")
+
+
+	}
+
+
+}
